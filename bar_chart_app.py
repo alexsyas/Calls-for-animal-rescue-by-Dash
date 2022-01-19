bb30f658491266a221a0c5b24d70eb852e2d26c2
Aleksander Syasin <57727966+alexsyas@users.noreply.github.com>
2022-01-19 18:48:58 +0300

    Add files via upload

---
+++ bar_chart_app.py
@@ -0,0 +1,108 @@
+import pandas as pd
+import datetime as dt
+import dash
+from dash import dcc
+from dash import html
+from dash.dependencies import Input, Output
+import plotly
+import plotly.express as px
+
+df = pd.read_csv("Urban_Park_Ranger_Animal_Condition_Response.csv")
+
+
+#-------------------------------------------------------------------------------
+# Исключение строк без животеых или вызовы с верифицированной возрастной группой
+
+df = df[(df["# of Animals"] > 0) & (df["Age"] != "Multiple")]
+
+df["Month of Initial Call"] = pd.to_datetime(df["Date and Time of initial call"])
+df["Month of Initial Call"] = df["Month of Initial Call"].dt.strftime("%m")
+
+df["Amount of Animals"] = df["# of Animals"]
+df["Time Spent on Site (hours)"] = df["Duration of Response"]
+#------------------------------------------------------------------------------------
+
+app = dash.Dash(__name__)
+
+app.layout = html.Div([
+
+        html.Div([
+            html.Pre(children= "NYC Calls for Animal Rescue",
+            style={"text-align": "center", "font-size": "100%", "color": "black"})
+        ]),
+
+        html.Div([
+            html.Label(["X-axis categories to compare:"],
+            style={"font-weight": "bold"}),
+            dcc.RadioItems(
+                id="xaxis_raditem",
+                options=[
+                    {"label": "Month Call Made", "value": "Month of Initial Call"},
+                    {"label": "Animal Health", "value": "Animal Condition"},
+                ],
+                value="Animal Condition",
+                style={"width" : "50%"}
+            ),
+        ]),
+
+        html.Div([
+            html.Br(),
+            html.Label(["Y-axis values to compare:"], style={"font-weight": "bold"}),
+            dcc.RadioItems(
+                id="yaxis_raditem",
+                options=[
+                        {"label": "Time Spent on Site (hours)", "value": "Time Spent on Site (hours)"},
+                        {"label": "Amount of Animals", "value": "Amount of Animals"},
+                ],
+                value="Time Spent on Site (hours)",
+                style={"width":"50%"}
+            ),
+        ]),
+
+
+    html.Div([
+        dcc.Graph(id="the_graph")
+    ]),
+
+])
+
+#-----------------------------------------------------
+
+@app.callback(
+    Output(component_id="the_graph", component_property="figure"),
+    [Input(component_id="xaxis_raditem", component_property="value"),
+     Input(component_id="yaxis_raditem", component_property="value")]
+)
+
+def update_graph(x_axis, y_axis):
+    dff = df
+    # print(dff[x_xis,y_axis]][:-1])
+
+    barchart=px.bar(
+        data_frame=dff,
+        x=x_axis,
+        y=y_axis,
+        title=y_axis + " : by " + x_axis,
+        color="Borough",
+        barmode="group")
+
+    barchart.update_layout(xaxis={"categoryorder": "total ascending"},
+                           title={"xanchor": "center", "yanchor": "top", "y": 0.9, "x": 0.5})
+    return barchart
+
+
+if __name__ == '__main__':
+    app.run_server(debug=True)
+
+
+
+
+
+
+
+
+
+
+
+
+
